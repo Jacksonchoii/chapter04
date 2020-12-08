aadc2260a5e857e88ebf9892df5172958423bc00
Jacksonchoii <cccctae@gmail.com>
2020-12-08 09:43:33 +0900

    chapter04

---
+++ src/com/javaex/ex01/CircleList.java
@@ -0,0 +1,34 @@
+package com.javaex.ex01;
+
+public class CircleList {
+
+	// 원
+
+	// 필드
+	private Circle[] cArray;
+	private int crtPos;
+
+	// 생성자
+	public CircleList() {
+		cArray = new Circle[3];
+		crtPos = 0;
+	}
+
+	// 메소드 getter setter 생략
+
+	// 일반 메소드
+
+	public void add(Circle c) {
+		cArray[crtPos] = c;
+		crtPos++;
+	}
+
+	public Circle get(int index) {
+		return cArray[index];
+	}
+
+	public int size() {
+		return crtPos;
+	}
+
+}
---
+++ src/com/javaex/ex01/RectList.java
@@ -0,0 +1,32 @@
+package com.javaex.ex01;
+
+public class RectList {
+
+	// 필드
+	private Rectangle[] rArray; // 배열을 담는 주소만 생김
+	private int crtPos; // 현재 포지션값 예시
+
+	// 생성자
+	public RectList() {
+		rArray = new Rectangle[3];
+		crtPos = 0; // 초기값 0으로 세팅 --> 0번째방
+	}
+
+	// 메소드 getter setter --> 필요없음
+
+	// 메소드 일반
+
+	public void add(Rectangle r) {
+		rArray[crtPos] = r;
+		crtPos++; // 증감식
+	}
+
+	public Rectangle get(int index) {
+		return rArray[index];
+	}
+
+	public int size() {
+		return crtPos;
+	}
+
+}
